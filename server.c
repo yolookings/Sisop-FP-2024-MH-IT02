5433460bf5eb4d6571731afc0ab0202ce4ca0aa7
bibliophile <151700439+khafhrnsaa@users.noreply.github.com>
2024-06-23 19:17:34 +0700

    Update server.c

--- server.c
+++ server.c
@@ -14,6 +14,7 @@ typedef struct {
     char username[50];
     char hashed_password[BCRYPT_HASHSIZE];
     char role[10];
+    int is_root;
 } User;
 
 User users[MAX_CLIENTS];
@@ -38,10 +39,7 @@ int main() {
     socklen_t addr_size = sizeof(struct sockaddr_in);
 
     server_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_sock < 0) {
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
-    }
+    if (server_sock < 0) { perror("Socket creation failed"); exit(EXIT_FAILURE); }
 
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
@@ -91,6 +89,7 @@ void handle_client(int client_sock) {
     while ((read_size = recv(client_sock, buffer, BUFFER_SIZE, 0)) > 0) {
         buffer[read_size] = '\0';
         char* command = strtok(buffer, " ");
+
         if (strcmp(command, "REGISTER") == 0) {
             char* username = strtok(NULL, " ");
             char* hashed_password = strtok(NULL, " ");
@@ -104,23 +103,88 @@ void handle_client(int client_sock) {
             char* username = strtok(NULL, " ");
             char* password = strtok(NULL, " ");
             if (login_user(username, password)) {
-                save_user_to_csv(username, password, "user");
                 send(client_sock, "LOGIN_SUCCESS", strlen("LOGIN_SUCCESS"), 0);
             } else {
                 send(client_sock, "LOGIN_FAILURE", strlen("LOGIN_FAILURE"), 0);
             }
         } else if (strcmp(command, "JOIN") == 0) {
-            // Implement JOIN command handling
             send(client_sock, "JOIN_SUCCESS", strlen("JOIN_SUCCESS"), 0);
         } else if (strcmp(command, "LIST") == 0) {
-            // Implement LIST command handling
-            send(client_sock, "LIST_RESPONSE", strlen("LIST_RESPONSE"), 0);
+            if (users[0].is_root == 1) {
+                char response[BUFFER_SIZE] = "[root] LIST USER\n";
+                for (int i = 0; i < user_count; i++) {
+                    strcat(response, users[i].username);
+                    strcat(response, " ");
+                }
+                send(client_sock, response, strlen(response), 0);
+            } else {
+                send(client_sock, "LIST_FAILURE", strlen("LIST_FAILURE"), 0);
+            }
         } else if (strcmp(command, "CHAT") == 0) {
-            // Implement CHAT command handling
             send(client_sock, "CHAT_SUCCESS", strlen("CHAT_SUCCESS"), 0);
         } else if (strcmp(command, "EDIT") == 0) {
-            // Implement EDIT command handling
-            send(client_sock, "EDIT_SUCCESS", strlen("EDIT_SUCCESS"), 0);
+            char* root_command = strtok(NULL, " ");
+            if (strcmp(root_command, "WHERE") == 0) {
+                char* target_username = strtok(NULL, " ");
+                char* option = strtok(NULL, " ");
+                
+                if (option != NULL && strcmp(option, "-u") == 0) {
+                    char* new_username = strtok(NULL, " ");
+                    if (new_username != NULL) {
+                        if (users[0].is_root == 1) {
+                            for (int i = 0; i < user_count; i++) {
+                                if (strcmp(users[i].username, target_username) == 0) {
+                                    strcpy(users[i].username, new_username);
+                                    send(client_sock, "EDIT_SUCCESS", strlen("EDIT_SUCCESS"), 0);
+                                    save_user_to_csv(target_username, users[i].hashed_password, users[i].role);
+                                    return;
+                                }
+                            }
+                        } else {
+                            send(client_sock, "EDIT_FAILURE", strlen("EDIT_FAILURE"), 0);
+                        }
+                    }
+                } else if (option != NULL && strcmp(option, "-p") == 0) {
+                    char* new_password = strtok(NULL, " ");
+                    if (new_password != NULL) {
+                        if (users[0].is_root == 1) {
+                            for (int i = 0; i < user_count; i++) {
+                                if (strcmp(users[i].username, target_username) == 0) {
+                                    bcrypt_hashpw(new_password, users[i].hashed_password);
+                                    send(client_sock, "EDIT_SUCCESS", strlen("EDIT_SUCCESS"), 0);
+                                    save_user_to_csv(users[i].username, users[i].hashed_password, users[i].role);
+                                    return;
+                                }
+                            }
+                        } else {
+                            send(client_sock, "EDIT_FAILURE", strlen("EDIT_FAILURE"), 0);
+                        }
+                    }
+                }
+            } else {
+                send(client_sock, "UNKNOWN_COMMAND", strlen("UNKNOWN_COMMAND"), 0);
+            }
+        } else if (strcmp(command, "REMOVE") == 0) {
+            char* target_username = strtok(NULL, " ");
+            if (users[0].is_root == 1) {
+                for (int i = 0; i < user_count; i++) {
+                    if (strcmp(users[i].username, target_username) == 0) {
+                        for (int j = i; j < user_count - 1; j++) {
+                            strcpy(users[j].username, users[j + 1].username);
+                            strcpy(users[j].hashed_password, users[j + 1].hashed_password);
+                            strcpy(users[j].role, users[j + 1].role);
+                            users[j].is_root = users[j + 1].is_root;
+                        }
+                        user_count--;
+                        send(client_sock, "REMOVE_SUCCESS", strlen("REMOVE_SUCCESS"), 0);
+                        save_user_to_csv(target_username, "", "");
+                        return;
+                    }
+                }
+                send(client_sock, "REMOVE_FAILURE", strlen("REMOVE_FAILURE"), 0);
+            } else {
+                send(client_sock, "REMOVE_FAILURE", strlen("REMOVE_FAILURE"), 0);
+            }
         } else {
             send(client_sock, "UNKNOWN_COMMAND", strlen("UNKNOWN_COMMAND"), 0);
         }
@@ -134,15 +198,16 @@ void handle_client(int client_sock) {
 }
 
 int register_user(const char* username, const char* hashed_password) {
-    for (int i = 0; i < user_count; i++) {
-        if (strcmp(users[i].username, username) == 0) {
-            return 0;
-        }
+    if (user_count == 0) {
+        strcpy(users[user_count].role, "root");
+        users[user_count].is_root = 1;
+    } else {
+        strcpy(users[user_count].role, "user");
+        users[user_count].is_root = 0;
     }
 
     strcpy(users[user_count].username, username);
     strcpy(users[user_count].hashed_password, hashed_password);
-    strcpy(users[user_count].role, "user");
     user_count++;
     return 1;
 }
@@ -160,10 +225,7 @@ int login_user(const char* username, const char* password) {
 
 void save_user_to_csv(const char* username, const char* hashed_password, const char* role) {
     FILE *fp = fopen("user.csv", "a");
-    if (fp == NULL) {
-        perror("Could not open user.csv");
-        return;
-    }
+    if (fp == NULL) { perror("Could not open user.csv"); return; }
     fprintf(fp, "%s,%s,%s\n", username, hashed_password, role);
     fclose(fp);
 }
